7d600af77492dddd5dbda79c5a90d6578a8ca712
Amjed <Amjed@DESKTOP-SUVDHH6>
2024-06-01 11:57:57 +0200

    JavaDoc

--- src/test/CompressionTest.java
+++ src/test/CompressionTest.java
@@ -11,7 +11,18 @@ import org.junit.jupiter.api.Test;
 
 import huffman.Compression;
 
+/**
+ * <p>
+ * Sert à tester la classe Compression, qui fournit 
+ * des fonctionnalités de compression utilisant l'algorithme 
+ * de Huffman. Les tests incluent la compression de texte,
+ * la gestion des cas d'erreur, et la recherche 
+ * de codes Huffman pour les caractères.
+ * </p>
+ */
 class CompressionTest {
+
+    // Définition des dictionnaires de test
 
     private static final Object[][] DICTIONNAIRE = { { 'c', "00" }, { 'o', "01" }, { 'u', "10" } };
 
@@ -19,6 +30,12 @@ class CompressionTest {
 
     private static final Object[][] DICTIONNAIRE_AVEC_NULL = { { 'a', "100" }, { null, null }, { 'c', "110" } };
 
+    /**
+     * <p>
+     * Test de compression d'un texte identique avec 
+     * un dictionnaire donné.
+     * </p>
+     */
     @Test
     void testCompressionTexteIdentique() {
 	String texteACompresser = "coucou";
@@ -29,6 +46,12 @@ class CompressionTest {
 	assertEquals(texteComprimeAttendu, texteComprime);
     }
 
+    /**
+     * <p>
+     * Test de gestion d'une exception lorsque le texte à compresser
+     * est vide.
+     * </p>
+     */
     @Test
     void testCompressionTexteVideException() {
 	assertThrows(IllegalArgumentException.class, () -> {
@@ -36,13 +59,24 @@ class CompressionTest {
 	});
     }
 
+    /**
+     * <p>
+     * Test de gestion d'une exception lorsque le texte à compresser
+     * est null.
+     * </p>
+     */
     @Test
     void testCompressionTexteNullException() {
 	assertThrows(IllegalArgumentException.class, () -> {
 	    Compression.compresserTexte(null, DICTIONNAIRE);
 	});
     }
-
+    
+    /**
+     * <p>
+     * Test de recherche du code Huffman pour des caractères présents dans le dictionnaire.
+     * </p>
+     */
     @Test
     void testTrouverCodeHuffmanCaracterePresent() {
 	assertEquals("00", Compression.trouverCodeHuffman('c', DICTIONNAIRE));
@@ -50,6 +84,12 @@ class CompressionTest {
 	assertEquals("10", Compression.trouverCodeHuffman('u', DICTIONNAIRE));
     }
 
+    /**
+     * <p>
+     * Test de recherche du code Huffman pour des caractères absents 
+     * du dictionnaire.
+     * </p>
+     */
     @Test
     void testTrouverCodeHuffmanCaractereAbsent() {
 	assertEquals("", Compression.trouverCodeHuffman('d', DICTIONNAIRE));
@@ -57,12 +97,24 @@ class CompressionTest {
 	assertEquals("", Compression.trouverCodeHuffman('w', DICTIONNAIRE));
     }
 
+    /**
+     * <p>
+     * Test de recherche du code Huffman dans un dictionnaire vide.
+     * </p>
+     */
     @Test
     void testTrouverCodeHuffmanDictionnaireVide() {
 	assertEquals("", Compression.trouverCodeHuffman('a', DICTIONNAIRE_VIDE));
 	assertEquals("", Compression.trouverCodeHuffman('f', DICTIONNAIRE_VIDE));
     }
 
+
+    /**
+     * <p>
+     * Test de recherche du code Huffman dans un dictionnaire 
+     * contenant des valeurs null.
+     * </p>
+     */
     @Test
     void testTrouverCodeHuffmanDictionnaireAvecNull() {
 	assertEquals("100", Compression.trouverCodeHuffman('a', DICTIONNAIRE_AVEC_NULL));

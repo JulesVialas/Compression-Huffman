cadde9b76727f1343482468c8999c3097a18630e
Amjed <Amjed@DESKTOP-SUVDHH6>
2024-06-01 11:18:58 +0200

    JavaDoc

--- src/huffman/Decompression.java
+++ src/huffman/Decompression.java
@@ -1,5 +1,5 @@
 /*
- * Decompression.java                                            02 mai 2024
+ * Decompression.java                                            22 mai 2024
  * IUT de Rodez, pas de copyright.
  */
 package huffman;
@@ -9,8 +9,15 @@ import java.io.IOException;
 import gestion.GestionFichierBinaire;
 
 /**
- * Decompression décompresse des fichiers de type text (.txt)
- */
+ * <p>Permet de décompresser des fichiers encodés en utilisant
+ * l'algorithme de Huffman.</p>
+ * <p>Les fichiers compressés sont décodés à l'aide d'un dictionnaire
+ * de caractères et de leurs encodages Huffman correspondants.</p>
+ * <p>Cette classe offre des méthodes pour lire le contenu 
+ * d'un fichier compressé, décompresser les données en utilisant
+ * le dictionnaire, puis retourner le texte décompressé.</p>
+ * @author LOUBIERE Landry, VIALAS Jules, SEHIL Amjed
+ */ 
 public class Decompression {
     /**
      * Décompresse des fichier à partir de l'encodage Huffman
